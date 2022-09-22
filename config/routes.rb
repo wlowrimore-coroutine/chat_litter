7ecae0687e353fd448ba73dd4b858fe67eeb6002
William Lowrimore <wlowrimore@gmail.com>
2022-09-22 17:36:35 -0500

    adds navbar with some content (not all)

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  
+
   root 'pages#home'
   get "rooms/index"
 end
