b72a9022a6a383bd8e08d7cd65c9dc5b3beaa939
William Lowrimore <wlowrimore@gmail.com>
2022-09-21 19:05:20 -0500

    adds basic sign-up, sign-in, and sign-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # -----------------------------------------------------
+  # Associations
+  # -----------------------------------------------------
+
   has_many :messages
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def username
+    return self.email.split('@')[0].capitalize
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   resources :rooms do
     resources :messages
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  root "rooms#index"
+  
+  root 'pages#home'
+  get "rooms/index"
 end
