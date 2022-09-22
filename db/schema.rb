bd04df2ab3128d345987c1986d368d35cf147691
William Lowrimore <wlowrimore@gmail.com>
2022-09-22 11:29:30 -0500

    adds user access with username signup and display

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_172919) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_145612) do
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
@@ -35,8 +35,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_172919) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "messages", "rooms"
